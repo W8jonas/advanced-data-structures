5de113c60e2ac67c7cfcb94f6e2dded50ec78e78
W8jonas <jonashenrique989@gmail.com>
2024-09-23 21:18:27 -0300

    Atualiza struct do film

--- source/types/film.h
+++ source/types/film.h
@@ -3,9 +3,23 @@
 
 typedef struct
 {
-  char movieName[51];
+  bool adult;
+  char genres[151];
+  unsigned int databaseId;
+  char imdbId[10];
+  char originalLanguage[5];
+  char originalTitle[51];
+  char overview[500];
   double popularity;
-  double vote_average;
+  char productionCompanies[50];
+  char productionCountries[50];
+  unsigned int yearOfRelease;
+  double revenue;
+  unsigned int runtime;
+  char spokenLanguages[50];
+  char title[50];
+  double voteAverage;
+  unsigned int voteCount;
 } Film;
 
 #endif // FILM_H
