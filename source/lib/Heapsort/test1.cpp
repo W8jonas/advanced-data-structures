6ae1534b781da33ca7ba23742d270c53fc286753
W8jonas <jonashenrique989@gmail.com>
2024-10-08 20:43:35 -0300

    Adiciona teste inicial para heapsort

---
+++ source/lib/Heapsort/test1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+#include "heapsort.h"
+#include "fileManager.h"
+#include "film.h"
+#include "metrics.h"
+
+int main()
+{
+    std::vector<int> numbers = {5, 3, 1, 4, 2};
+    Heapsort<int>::heapsort(numbers);
+    assert(numbers[0] == 5);
+    assert(numbers[1] == 4);
+    assert(numbers[2] == 3);
+    assert(numbers[3] == 2);
+    assert(numbers[4] == 1);
+    return 0;
+}
