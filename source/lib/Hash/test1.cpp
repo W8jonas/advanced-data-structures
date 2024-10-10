17e32b042854ca83f9a7ec2c8b0fbfdec6b8a5ce
W8jonas <jonashenrique989@gmail.com>
2024-10-10 20:34:12 -0300

    Cria primeiro teste de Hash

--- source/lib/Hash/test1.cpp
+++ source/lib/Hash/test1.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 #include <string>
+#include "hash.h"
+#include <cassert>
 
 // Testa Hash
 int main()
 {
+  HashTable hashTable(5);
+
+  hashTable.insert("a");
+  hashTable.insert("a");
+  hashTable.insert("a");
+
+  auto entries = hashTable.getAllEntries();
+
+  assert(entries[0].first == "a");
+  assert(entries[0].second == 3);
+
   return 0;
 }
